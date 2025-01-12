60d20625e81f4a85d4e0a74b8a647f461c694f8f
Simu-He <2530606006@qq.com>
2025-01-12 22:09:53 +0900

    first log update

--- q1.py
+++ q1.py
@@ -1,2 +1,48 @@
-print("hello world")
-print("hello world 2")
+import logging
+from datetime import datetime
+from functools import wraps
+import os
+
+log_directory = "logs"
+os.makedirs(log_directory, exist_ok=True)
+
+
+
+logging.basicConfig(
+    filename=os.path.join(log_directory, "action_log.log"),
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+
+def log_and_execute(human_id, time, location, action_id, *action_params):
+    
+
+    @wraps(action_id)
+    def wrapped_action(*args, **kwargs):
+        try:
+            action_function = action_list[action_id]
+            logging.info(
+                f"Action executed: {action_function.__name__} | "
+                f"Human ID: {human_id}, Time: {time}, Location: {location}"
+            )
+
+            return action_function(human_id, time, location, *action_params)
+        except Exception as e:
+            logging.error(f"Error during action execution: {e}")
+            raise
+
+    wrapped_action()
+
+
+
+if __name__ == "__main__":
+    human_id = "4"
+    x = 3
+    time = datetime(2024,4,x,16,44,44)
+    assert x > 0 and x < 15
+    location = "ijigen shokoko"
+
+
+    result = log_and_execute(human_id, time, location, 3,"happy new year")
